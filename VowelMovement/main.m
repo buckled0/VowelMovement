dc9693ebda69803d52599046a8f2bba6eeb4b07d
Daniel Buckle <danielbuckle31@gmail.com>
2013-04-22 17:56:54 +0100

    Initial Commit

---
+++ VowelMovement/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  VowelMovement
+//
+//  Created by Daniel Buckle on 22/04/2013.
+//  Copyright (c) 2013 Daniel Buckle. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
